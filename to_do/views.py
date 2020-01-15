3e9f80440490c97415f622223d768fd31b3fa010
Joseph Hangarter <joseph.hangarter@yahoo.com>
2020-01-15 14:37:34 -0800

    got something to render on page

--- to_do/views.py
+++ to_do/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Post
 
-# Create your views here.
+class HomePageView(ListView):
+    template_name = 'home.html'
+    model = Post
+    context_object_name = 'posts'
+
+class ToDoDetailsView(DetailView):
+    template_name = 'to_do_details.html'
+    model = Post
