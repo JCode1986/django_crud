9c9053fb098e2bfaab118ca4925c63ab372609b7
Joseph Hangarter <joseph.hangarter@yahoo.com>
2020-01-20 08:40:19 -0800

    update and delete functionality working

--- to_do/urls.py
+++ to_do/urls.py
@@ -4,7 +4,7 @@ from .views import HomePageView, ToDoDetailsView, ToDoDeleteView, ToDoCreateView
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
     path('post/<int:pk>/', ToDoDetailsView.as_view(), name='to_do_details'),
-    path('to_do/delete/', ToDoDeleteView.as_view(), name='to_do_delete'),
+    path('to_do/delete/<int:pk>/', ToDoDeleteView.as_view(), name='to_do_delete'),
     path('to_do/create/', ToDoCreateView.as_view(), name='to_do_create'),
-    path('to_do/update/', ToDoUpdateView.as_view(), name='to_do_update'),
+    path('to_do/update/<int:pk>/', ToDoUpdateView.as_view(), name='to_do_update'),
 ]
